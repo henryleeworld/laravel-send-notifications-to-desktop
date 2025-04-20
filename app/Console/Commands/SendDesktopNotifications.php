b56acf8ddb9dc07d6b36727a6c39fe4a004f2e24
Henry Lee <henryleeworld@gmail.com>
2025-04-21 00:09:01 +0800

    feat:升級至 laravel 11

--- app/Console/Commands/SendDesktopNotifications.php
+++ app/Console/Commands/SendDesktopNotifications.php
@@ -18,26 +18,13 @@ class SendDesktopNotifications extends Command
      *
      * @var string
      */
-    protected $description = '桌面通知';
-
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+    protected $description = 'Desktop notification';
 
     /**
      * Execute the console command.
-     *
-     * @return int
      */
     public function handle()
     {
-        // $this->notify('您好，網路 Artisan', '喜歡代碼之美嗎？我們也這樣做！');
-        $this->notify(config('app.name'), '等您好久，歡迎加入👋。', storage_path('images/kamen-rider-black.png'));
+        $this->notify(config('app.name'), __('Waiting for you for a long time, welcome to join👋.'), storage_path('images/kamen-rider-black.png'));
     }
 }
